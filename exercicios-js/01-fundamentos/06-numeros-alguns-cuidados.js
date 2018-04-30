7dc41e735c9bc63aa531fc6ac6ccf2d71f6b4dde
Rafael Marques <rafaelmarques1992@gmail.com>
2018-04-30 11:37:53 -0300

    Number - Alguns cuidados

---
+++ exercicios-js/01-fundamentos/06-numeros-alguns-cuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0); // Retorna infinito
+console.log('10' / 2); // Funciona normalmente
+console.log('Show!' * 2); // Não é um número
+console.log(0.1 + 0.7); // Imprecisão
+// console.log(10.toString()); Não funciona
+console.log((10.345).toFixed(2)); // Funciona normalmente
