0ef7a9cce79c1de4bba57b0d2be004499fcb3b62
Rafael Marques <rafaelmarques1992@gmail.com>
2018-04-30 10:21:46 -0300

    Explicação de quais são os tipos de variáveis

---
+++ exercicios-js/01-fundamentos/03-variaveis-e-constantes.js
@@ -0,0 +1,16 @@
+var a = 3; // 1 sinal de '=' significa atribuição
+let b = 4; 
+
+var a = 30;
+b = 40;
+
+console.log(a, b);
+
+a = 300;
+b = 400;
+
+console.log(a, b);
+
+const c = 5;
+// c = 50 nesse caso não é possível reatribuir variável do tipo constante
+console.log(c);
