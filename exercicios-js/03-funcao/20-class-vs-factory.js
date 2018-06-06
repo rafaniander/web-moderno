f28f4a3d7e3477344c03cdb17b61847c8c258514
Rafael Marques <rafaelmarques1992@gmail.com>
2018-06-05 21:07:58 -0300

    Classe vs Função Factory

---
+++ exercicios-js/03-funcao/20-class-vs-factory.js
@@ -0,0 +1,23 @@
+// Classe
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    falar() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
+
+// Factory
+const criarPessoa = nome => {
+    return {
+        falar: () => console.log(`Meu nome é ${nome}`)
+    }
+}
+
+const p2 = criarPessoa('Pedro')
+p2.falar()
